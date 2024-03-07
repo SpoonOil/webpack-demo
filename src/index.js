7e0ee71ed30a534f3bde3990e36e474272aa5a0a
SpoonOil <69192866+SpoonOil@users.noreply.github.com>
2024-03-07 19:21:36 +0000

    finish project!

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import _ from 'lodash';
 import './style.css';
+import printMe from './print.js'
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button')
 
     //lodash, now imported by this script
     element.innerHTML = _.join(['Hello,', 'webpack'], ' ')
     element.classList.add('hello')
+
+    btn.innerText = 'Click me and check the console';
+    btn.onclick = printMe;
+    
+    element.appendChild(btn)
 
     return element
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,23 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename:'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js',
     },
+    mode: 'development',
+    output: {
+        filename:'[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+        }),
+    ],
     module: {
         rules: [
             {
